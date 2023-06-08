d6722ee060e85bd0394d60e38fbfb121f69819cd
unknown <zhongyl1@wangsu.com>
2023-06-08 15:07:19 +0800

    1

---
+++ Stream.py
@@ -0,0 +1,41 @@
+from scapy.all import *
+# 数据流类
+
+
+class Stream:
+    def __init__(self, pkt):
+        self.pkts = [pkt]  # 数据包列表
+        self.src_ip = pkt[IP].src  # 源IP地址
+        self.dst_ip = pkt[IP].dst  # 目的IP地址
+        self.sport = pkt.sport  # 源端口号
+        self.dport = pkt.dport  # 目的端口号
+        self.protocol = pkt[IP].proto  # 协议id
+        self.content = [pkt.load.decode(errors='ignore')] if pkt.haslayer(
+            'Raw') else []  # 数据流内容列表
+        self.prnames = pkt[IP].payload.name
+
+    def update(self, pkt):  # 更新流
+        self.pkts.append(pkt)
+        # 如果数据包有原始层，则将原始数据解码并添加到流内容中
+        if pkt.haslayer('Raw'):
+            self.content.append(pkt.load.decode(errors='ignore'))
+
+def parse_pcap(file_name):
+    streams = {}
+    pkts = rdpcap(file_name)  # 读取pcap文件
+    for pkt in pkts:
+        if IP in pkt:  # 如果该包包含 IP 层信息
+            proto = pkt[IP].proto  # 获取协议编号
+            if proto in [6, 17, 132, 33, 46]:  # 如果是TCP\UDP\SCTP\DCCP\RSVP\QUIC
+                five_tuple = None
+                if proto == 6:
+                    five_tuple = (pkt[IP].src, pkt[IP].dst,
+                                  pkt[TCP].sport, pkt[TCP].dport, proto)
+                elif proto == 17:
+                    five_tuple = (pkt[IP].src, pkt[IP].dst,
+                                  pkt[UDP].sport, pkt[UDP].dport, proto)
+                if five_tuple in streams:  # 如果该五元组已经存在于streams字典中
+                    streams[five_tuple].update(pkt)  # 更新流
+                else:
+                    streams[five_tuple] = Stream(pkt)  # 创建新的流
+    return streams
--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 from flask import Flask, render_template, request
 from scapy.all import *
-from flows import parse_pcap
+from Stream import parse_pcap
 app = Flask(__name__)
 
-# 首页，上传pcap文件
+# 首页
 @app.route("/")
 def index():
     return render_template("index.html")
@@ -13,10 +13,8 @@ def index():
 def process_file():
     # 获取上传的文件
     file = request.files["file"]
-
-    # 解析pcap文件
+    # 解析上传的pcap文件，将每个数据流保存为Stream对象，并以五元组为键值存储到字典streams中
     streams = parse_pcap(file)
-
     # 将流的信息传入html页面展示
     return render_template("result.html", streams=streams)
 
